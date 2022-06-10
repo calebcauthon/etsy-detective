4e59208af41b7bb906463fed812b944bdf2ec93b
Caleb <calebcauthon@gmail.com>
2022-06-10 09:51:20 -0500

    test

--- src/routes.js
+++ src/routes.js
@@ -2,47 +2,21 @@ const Apify = require('apify');
 
 const { utils: { log } } = Apify;
 
-async function getListingLinksFromSearchResults(page) {
-    console.log('getting links.');
+exports.handleStart = async ({ request, page }) => {
     var result = await page.evaluate(() => {
         function superTrim(text) {
             text = text.replaceAll('\n', '').trim();
             return text;
         }
         
-        var linkSelector = 'a[href*=listing]';
-        var listingLinks = $(linkSelector).map((index, el) => {
-            const listingLink = el.href;
-
-//
-//            return {
-//                text: superTrim($(el).find('h3').text()),
-//                listing_href: el.href,
-//                seller:
-//            };
-            return listingLink;
+        var listings = $('a[href*=listing]').map((index, el) => {
+            return {
+                text: superTrim($(el).find('h3').text()),
+                href: el.href
+            };
         }).get()
-        return Promise.resolve(listingLinks);
+        return Promise.resolve(listings);
     });
-}
-
-
-exports.handleStart = async ({ request, page }) => {
-    var links = await getListingLinksFromSearchResults(page);    
-    console.log("listings found: ", links)
-
-    const link = links[0];
-    console.log("opening page: ${link}");
-    const listingPage = await browser.newPage();
-    await listingPage.goto(link);
-
-    var sellerName = await listingPage.evaluate(() => {
-        const sellerName = "test"
-        return sellerName;
-    });
-
-    console.log(`seller name is ${sellerName}`);
-
     console.log("done with handle start", result)
 };
 
