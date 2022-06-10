8a5c29dc2cabd0fa605e319f27cb9dcca79e9ebe
Caleb <calebcauthon@gmail.com>
2022-06-10 09:18:35 -0500

    Initial commit from online code editor

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const Apify = require('apify');
+
+const { utils: { log } } = Apify;
+
+exports.handleStart = async ({ request, page }) => {
+    var result = await page.evaluate(() => {
+        function superTrim(text) {
+            text = text.replaceAll('\n', '').trim();
+            return text;
+        }
+        
+        var listings = $('a[href*=listing]').map((index, el) => {
+            return {
+                text: superTrim($(el).find('h3').text()),
+                href: el.href
+            };
+        }).get()
+        return Promise.resolve(listings);
+    });
+    console.log("done with handle start", result)
+};
+
+exports.handleList = async ({ request, page }) => {
+    // Handle pagination
+};
+
+exports.handleDetail = async ({ request, page }) => {
+    // Handle details
+};
